bb5e69e742e407542771a25e646102647c7fa65d
MariaE-Santos <92039695+MariaE-Santos@users.noreply.github.com>
2024-06-17 20:37:35 -0300

    test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+def main(lista):  # Não alterar esta linha
+    # Início da resposta
+    soma = sum(lista)
+    media = soma/len(lista)
+        
+    # Fim da resposta
+    return media  # Não alterar esta linha
